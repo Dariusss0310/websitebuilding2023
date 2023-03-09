6cc6924baabebfd9e4f5aba5c2dcdc4c88fe6fd4
Frontend JavaScript <frontendjavascript@C3-12.local>
2023-03-09 20:49:01 +0200

    curs4

--- shop/assets/js/main.js
+++ shop/assets/js/main.js
@@ -7,29 +7,51 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   touch: true,
   ride: false
 })
+// go to top
+
+const scrollBtn = document.querySelector(".gototop");
+const btnVisibility = () => {
+  if (window.scrollY > 400) {
+    scrollBtn.style.visibility = "visible";
+  } else {
+    scrollBtn.style.visibility = "hidden";
+  }
+};
+document.addEventListener("scroll", () => {
+  btnVisibility();
+});
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
 
-var id;
-$("#modalForm").on('show.bs.modal', function(event) {
+
+
+  var id;
+  $("#modalForm").on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); //Button that triggered the modal
-    const email = button.data('email') //need to find the button and get email
-    const nume = button.data('nume') //need to find the button and get name
-    $(".modal-body #email").val( email );
-    $(".modal-body #name").val( nume );
+    const produs = button.data('name') //need to find the button and get product
+    const pret = button.data('price') //need to find the button and get price
+    const description = button.data('description');
+    $(".modal-header #titleModalLabel").html(produs);
+    $(".modal-body #price").html(pret);
+    $(".modal-body #description").html(description);
     id = button.attr('id');
     // var datainfo = document.getElementById(id);
-     alert(JSON.stringify(button[0]));
+    alert(JSON.stringify(button));
     // alert(JSON.stringify(datainfo));
-    
-   
+
+
   })
-  $("#modalForm").on('hidden.bs.modal', function(event) {
-   
-    alert('Modalul s-a inchis');
+  $("#modalForm").on('hidden.bs.modal', function (event) {
+
+    // alert('Modalul s-a inchis');
     document.getElementById(id).classList.remove('btn-primary');
-    document.getElementById(id).classList.add('btn-success');
+    document.getElementById(id).classList.add('btn-info');
   })
-  
 document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
         if (window.scrollY > 400) {
